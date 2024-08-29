5c08292500967b86d8cf80e8ab4d34abd956260a
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2024-08-29 18:48:49 +0530

    Update ResetPasswordPage.jsx

--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-// import { BarLoader } from 'react-spinners';
 import LoadingBar from '../components/LoadingBar';
 
 const apiUrl = process.env.REACT_APP_API_URL;
