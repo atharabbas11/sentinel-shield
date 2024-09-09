d9e5321c99b0dcd87db9ca5251d3713a044599ee
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2024-09-09 21:51:45 +0530

    Update FeaturesPage.jsx

--- frontend/src/pages/FeaturesPage.jsx
+++ frontend/src/pages/FeaturesPage.jsx
@@ -4,7 +4,6 @@ import backgroundImage from '../images/bg-1.png';
 import feature1 from '../images/feature-1.jpg';
 import feature2 from '../images/feature-2.jpg';
 import feature3 from '../images/feature-3.jpg';
-// import LoadingBar from '../components/LoadingBar'; // Ensure the path is correct based on your project structure
 import { IoLockClosedOutline } from "react-icons/io5";
 import { VscSymbolInterface } from "react-icons/vsc";
 import { FcDataProtection } from "react-icons/fc";
@@ -13,7 +12,6 @@ import { FcDataProtection } from "react-icons/fc";
 const FeaturesPage = () => {
   return (
     <div className="min-h-screen relative bg-custom-bg">
-      {/* <LoadingBar loading={loading} /> */}
       <section style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'bottom', }}>
         <main className="relative z-10 container mx-auto mt-8 p-4">
           <div className="relative overflow-hidden mt-20 pb-32 space-y-24">
@@ -32,7 +30,6 @@ const FeaturesPage = () => {
                       <h2 className="text-3xl font-bold tracking-tight text-white">Secure Encryption :</h2>
                       <p className="mt-4 text-lg text-gray-300">Encrypt your messages with advanced algorithms to ensure complete privacy and security.</p>
                       <div className="mt-6">
-                        {/* <a className="inline-flex rounded-lg bg-pink-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-pink-600 hover:bg-pink-700 hover:ring-pink-700" href="#">Learn More</a> */}
                         <button className="inline-flex justify-end rounded bg-blue-600 text-white px-4 py-2 text-base font-semibold leading-7 hover:bg-blue-700 " href="#">Learn More</button>
                       </div>
                     </div>
@@ -45,7 +42,6 @@ const FeaturesPage = () => {
                       width="647" height="486"
                       style={{ color: "transparent" }}
                       className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                      // src="https://images.unsplash.com/photo-1569144157591-c60f3f82f137"
                       src={feature1}
                       alt='feature1'
                     />
@@ -81,7 +77,6 @@ const FeaturesPage = () => {
                       width="647" height="486"
                       style={{ color: "transparent" }}
                       className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                      // src="https://images.unsplash.com/photo-1599134842279-fe807d23316e"
                       src={feature2}
                       alt='feature2'
                     />
@@ -117,7 +112,6 @@ const FeaturesPage = () => {
                       width="647" height="486"
                       style={{ color: "transparent" }}
                       className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                      // src="https://images.unsplash.com/photo-1483478550801-ceba5fe50e8e"
                       src={feature3}
                       alt='feature3'
                     />
